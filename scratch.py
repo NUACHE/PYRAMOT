cba6afe550537f165e8d996b570951a5dc20bc5a
NUACHE <michaelopoku790@gmail.com>
2020-08-07 18:31:30 +0000

    test for location

--- scratch.py
+++ scratch.py
@@ -1,13 +1,24 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+# from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import requests
-import re
+from requests import get
+
+
+# import re
 
 
 def get_url():
     contents = requests.get('https://api.covid19api.com/summary').json()
     # print(contents["Countries"])
+
+    
+
+    globalContent = contents["Global"]
     want = contents["Countries"]
     print(want[1]['NewConfirmed'])
+    choice = input("Enter what you want")
+    if choice == 'globalContent':
+        print(globalContent['NewConfirmed'])
+
     # return url
 
 
